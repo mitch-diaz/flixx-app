f29e6e5600da0912324c53946c4f4a7927e75dde
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2023-03-25 22:06:55 -0400

    Added swiper functionality

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const global = {
 
 // Display 20 popular movies
 async function displayPopularMovies() {
-  const {results} = await fetchAPIData('movie/popular');
+  const { results } = await fetchAPIData('movie/popular');
   results.forEach((movie) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -197,7 +197,6 @@ async function displayShowDetails() {
 }
 
 
-
 // Display backdrop on movie details page
 function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement('div');
@@ -218,6 +217,54 @@ function displayBackgroundImage(type, backgroundPath) {
   } else {
     document.querySelector('#show-details').appendChild(overlayDiv);
   }
+}
+
+
+// Display Slider Movies
+async function displaySlider() {
+  const { results } = await fetchAPIData('movie/now_playing');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+      </h4>   
+    `;
+
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  })
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoPlay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2
+      },
+      700: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
+  });
 }
 
 
@@ -270,6 +317,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
+      displaySlider();
       displayPopularMovies();
       break;
     case '/shows.html':
